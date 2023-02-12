80dfc4f82d97ebe13b00b1ec87287dfd84b7959e
krbtsv <maxim.karabtsov@gmail.com>
2023-02-12 15:44:24 +0300

    [udp] Friend or Foe?

--- python-solutions/katas/kyu_7/friend_or_foe.py
+++ python-solutions/katas/kyu_7/friend_or_foe.py
@@ -1,2 +1,12 @@
 def friend(x: list[str]) -> list[str]:
     return [friend for friend in x if len(friend) == 4]
+
+
+def friend_second_solution(x: list[str]) -> list[str]:
+    import re
+
+    true_friends = []
+    for name in x:
+        if len(name) == 4 and re.search('[a-zA-Z]', name):
+            true_friends.append(name)
+    return true_friends
--- python-solutions/tests/kyu_7_tests/test_friend_or_foe.py
+++ python-solutions/tests/kyu_7_tests/test_friend_or_foe.py
@@ -1,6 +1,6 @@
 import unittest
 
-from katas.kyu_7.friend_or_foe import friend
+from katas.kyu_7.friend_or_foe import friend, friend_second_solution
 
 
 class FriendTestCase(unittest.TestCase):
@@ -8,4 +8,10 @@ class FriendTestCase(unittest.TestCase):
         self.assertEqual(friend(["Ryan", "Kieran", "Mark", ]), ["Ryan", "Mark"])
         self.assertEqual(friend(["Ryan", "Jimmy", "123", "4", "Cool Man"]), ["Ryan"])
         self.assertEqual(friend(["Jimm", "Cari", "aret", "truehdnviegkwgvke", "sixtyiscooooool"]),
+                         ["Jimm", "Cari", "aret"])
+
+    def test_friend_second_solution(self):
+        self.assertEqual(friend_second_solution(["Ryan", "Kieran", "Mark", ]), ["Ryan", "Mark"])
+        self.assertEqual(friend_second_solution(["Ryan", "Jimmy", "123", "4", "Cool Man"]), ["Ryan"])
+        self.assertEqual(friend_second_solution(["Jimm", "Cari", "aret", "truehdnviegkwgvke", "sixtyiscooooool"]),
                          ["Jimm", "Cari", "aret"])
