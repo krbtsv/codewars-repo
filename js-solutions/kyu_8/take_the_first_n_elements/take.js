e0fff5cb9723c68648c8ce534470fd3ce96e3dbd
Maxim <104375717+krbtsv@users.noreply.github.com>
2024-07-06 16:21:06 +0300

    [add]

---
+++ js-solutions/kyu_8/take_the_first_n_elements/take.js
@@ -0,0 +1,11 @@
+function take(arr, num) {
+    let firstNumElementsOfArr = [];
+
+    for (let i = 0; i < num; i++) {
+        firstNumElementsOfArr.push(arr[i]);
+    }
+
+    return firstNumElementsOfArr;
+}
+
+console.log(take([1,2,3,4,5], 3));
